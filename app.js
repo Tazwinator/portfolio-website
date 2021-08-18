6f8263a8a7d4fc4ad69f4fa472366bec4a119d6f
Tazwinator <taylor_stevenson@yahoo.co.uk>
2021-08-18 23:15:09 +0100

    Worked on the data page. In the process of doing the backend for the image upload.

--- app.js
+++ app.js
@@ -1,78 +1,84 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-};
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const axios = require("axios");
-const { catchAsyncErr, isLoggedIn } = require("./node-files/middleware");
-const users = require("./node-files/userControllers");
+const path = require('path');
+const axios = require('axios');
+const { catchAsyncErr, isLoggedIn } = require('./node-files/middleware');
+const users = require('./controllers/users');
 
-// Session / Cookies
-const session = require("express-session");
-const MongoDBStore = require("connect-mongo");
+// --------- Session / Cookies --------------------------- //
+const session = require('express-session');
+const MongoDBStore = require('connect-mongo');
 
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
+// --------- Cloudinary & Multer --------------------------- //
+
+const multer = require('multer');
+const { storage } = require('./cloudinary');
+const upload = multer({ storage });
 
 // --------- Static Files --------------------------- //
-const ejsMate = require("ejs-mate");
-app.set("view engine", "ejs");
-app.engine("ejs", ejsMate)
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")))
+const ejsMate = require('ejs-mate');
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({extended: true}));
+// --------- Parsing POST data --------------------------- //
+
+app.use(express.urlencoded({ extended: true }));
 
 // --------- Mongo --------------------------- //
 
-const Mongoose = require("mongoose");
+const Mongoose = require('mongoose');
 
-const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL;
 
 Mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useCreateIndex: true, 
-  useUnifiedTopology: true,
-  useFindAndModify: false // This one fixes a deprication error with findByIdAndDelete
+	useNewUrlParser    : true,
+	useCreateIndex     : true,
+	useUnifiedTopology : true,
+	useFindAndModify   : false // This one fixes a deprication error with findByIdAndDelete
 });
 
 const db = Mongoose.connection;
-db.on("error", console.error.bind("console", "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
+db.on('error', console.error.bind('console', 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected');
 });
-
 
 // --------- Session --------------------------- //
 
+const secret = process.env.SECRET || 'thishouldbeabettersecret!';
 
-
-const secret = process.env.SECRET || "thishouldbeabettersecret!";
-
-const store = new MongoDBStore ({
-  mongoUrl: dbUrl,
-  secret: secret,
-  touchAfter: 24 * 60 * 60 // In seconds.
+const store = new MongoDBStore({
+	mongoUrl   : dbUrl,
+	secret     : secret,
+	touchAfter : 24 * 60 * 60 // In seconds.
 });
 
-store.on("error", function (e) {
-  console.log("SESSION STORE ERROR", e)
+store.on('error', function (e) {
+	console.log('SESSION STORE ERROR', e);
 });
 
-const sessionConfig = { // Sets up the sessionId cookie
-  store,
-  name: "SID",
-  secret: secret, // this and SHA256 encrypt the cookie
-  resave: false,
-  saveUninitialized: false,
-  cookie: { // cookie attributes
-    httpOnly: true,
-    // secure: true; ONLY SETS SESSION COOKIES ON SECURE CONNECTIONS (HTTPS), DOES NOT WORK ON LOCALHOST!!!!!!!!!!!!
-    expires: Date.now() + 1000 * 60 * 60 *24 * 7, // Milliseconds
-    maxAge: 1000 * 60 * 60 *24 * 7
-  }
+const sessionConfig = {
+	// Sets up the sessionId cookie
+	store,
+	name              : 'SID',
+	secret            : secret, // this and SHA256 encrypt the cookie
+	resave            : false,
+	saveUninitialized : false,
+	cookie            : {
+		// cookie attributes
+		httpOnly : true,
+		// secure: true; ONLY SETS SESSION COOKIES ON SECURE CONNECTIONS (HTTPS), DOES NOT WORK ON LOCALHOST!!!!!!!!!!!!
+		expires  : Date.now() + 1000 * 60 * 60 * 24 * 7, // Milliseconds
+		maxAge   : 1000 * 60 * 60 * 24 * 7
+	}
 };
 app.use(session(sessionConfig));
 
@@ -81,37 +87,36 @@ app.use(session(sessionConfig));
 app.use(cookieParser());
 
 const getGeoData = async (ip) => {
-  try {
-      const config = { headers: { accept: 'application/json' } }
-      const res = await axios.get(`https://freegeoip.app/json/${ip}`, config)
-      return res.data
-  } catch (e) {
-      console.log("GeoIp not worked", e)
-  }
+	try {
+		const config = { headers: { accept: 'application/json' } };
+		const res = await axios.get(`https://freegeoip.app/json/${ip}`, config);
+		return res.data;
+	} catch (e) {
+		console.log('GeoIp not worked', e);
+	}
 };
 
 app.use(async (req, res, next) => {
-  if (!req.cookies.GeoIp) {
-    const ip = req.socket.remoteAddress;
-    const data = await getGeoData(ip);
-    const geo = `${data.country_code}/${data.region_code}/${data.city}/${data.zip_code}/IP:${data.ip}`
-    console.log(geo);
-    res.cookie("GeoIp", geo, {
-      httpOnly: true,
-      expires: Date.now() + 1000 * 60 * 60 *24 * 7, // In Milliseconds
-      maxAge: 1000 * 60 * 60 *24 * 7 // In milliseconds
-    });
-    next();
-  }
-  next();
+	if (!req.cookies.GeoIp) {
+		const ip = req.socket.remoteAddress;
+		const data = await getGeoData(ip);
+		const geo = `${data.country_code}/${data.region_code}/${data.city}/${data.zip_code}/IP:${data.ip}`;
+		console.log(geo);
+		res.cookie('GeoIp', geo, {
+			httpOnly : true,
+			expires  : Date.now() + 1000 * 60 * 60 * 24 * 7, // In Milliseconds
+			maxAge   : 1000 * 60 * 60 * 24 * 7 // In milliseconds
+		});
+		next();
+	}
+	next();
 });
 
 // --------- Passport --------------------------- //
 
-
-const passport = require("passport");
-const passportLocal = require("passport-local");
-const User = require("./node-files/userModel");
+const passport = require('passport');
+const passportLocal = require('passport-local');
+const User = require('./node-files/userModel');
 
 app.use(passport.initialize()); // Sets up passport
 app.use(passport.session()); // Makes it use the express-session for persistant sessions
@@ -123,74 +128,78 @@ passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
 // --------- FLash --------------------------- //
 
-const flash = require("connect-flash");
+const flash = require('connect-flash');
 
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
 // --------- Other (also to play around with and test stuff) --------------------------- //
 
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user; // req.user is set by passport
-  req.session.returnTo = req.header("Referrer");
-  next();
+	res.locals.currentUser = req.user; // req.user is set by passport
+	req.session.returnTo = req.header('Referrer');
+	/* 	console.log(req.user);
+ */ next();
 });
-
 
 // ----------------------- Routes ------------------------------- //
 
-app.get("/", (req, res) => { // Home
-  res.render("pages/home")
+// Home
+app.get('/', (req, res) => {
+	res.render('pages/home');
 });
 
-app.route("/auth") // auth page get
-.get((req, res) => {
-  res.render("pages/auth")
+// auth page get
+app.route('/auth').get((req, res) => {
+	res.render('pages/auth');
 });
 
 // User auth routes
-app.route("/register")
-  .post(catchAsyncErr(users.registerSubmit));
+app.route('/register').post(catchAsyncErr(users.registerSubmit));
 
-app.route("/login")
-  .post(passport.authenticate("local", { failureFlash: true, failureRedirect: "/auth"}), users.loginSubmit);
+app.route('/login').post(
+	passport.authenticate('local', {
+		failureFlash    : true,
+		failureRedirect : '/auth'
+	}),
+	users.loginSubmit
+);
 
-app.get("/logout", isLoggedIn, users.logout);
+app.get('/logout', isLoggedIn, users.logout);
 
 // Data Uplaod routes
 
-app.route("/data")
-.get((req, res) => {
-  res.render("pages/data")
+app.route('/data').get((req, res) => {
+	res.render('pages/data');
 });
+
+app.route('/img-data').post(upload.single('image'), (req, res) => {
+	console.dir(req.file);
+});
+
+/* app.route('/txt-data').post(); */
 
 // API routes
 
-app.route("/api")
-.get((req, res) => {
-  res.render("pages/api")
+app.route('/api').get((req, res) => {
+	res.render('pages/api');
 });
 
 // Tech stack routes
 
-app.route("/tech")
-.get((req, res) => {
-  res.render("pages/tech")
+app.route('/tech').get((req, res) => {
+	res.render('pages/tech');
 });
 
-
-
 app.listen(3000, () => {
-  console.log("Listening on port 3000")
+	console.log('Listening on port 3000');
 });
 
 // Plan:
---
+++ controllers/users.js
@@ -0,0 +1,33 @@
+const User = require('../node-files/userModel');
+
+module.exports.registerSubmit = async (req, res) => {
+	try {
+		const { email, username, password } = req.body;
+		const user = new User({ email, username });
+		const registeredUser = await User.register(user, password); // Passport's method for hashing and salting
+		console.log(registeredUser); // ------------------- For show, delete when necessary
+		req.login(registeredUser, (err) => {
+			if (err) return next(err);
+			req.flash('success', 'Registered and logged in, welcome!');
+			res.redirect('/auth');
+		});
+	} catch (e) {
+		req.flash('error', e.message);
+		res.redirect('/auth');
+	}
+};
+
+module.exports.loginSubmit = (req, res) => {
+	req.flash('success', 'Logged in, welcome!'); // Passed in to the next request
+	const redirectUrl = req.session.returnTo || '/auth';
+	delete req.session.returnTo;
+	res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res) => {
+	req.logout();
+	req.flash('success', 'Logged out, goodbye!');
+	const redirectUrl = req.session.returnTo || '/auth';
+	delete req.session.returnTo;
+	res.redirect(redirectUrl);
+};
